d5941fd8f413270f9bda480ec0d9ae87e875f495
Pochon Levit Floriane BFS <floriane.pochonlevit@bfs.admin.ch>
2025-10-20 11:38:36 +0200

    added prd workflow

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # harvester_SPHN
 
-This is a test harvester of the SPHN FDP catalog to I14Y ABN (test) environment.
+This is a test harvester of the SPHN FDP catalog to I14Y ABN (test) and PRD environments.
--- src/config.py
+++ src/config.py
@@ -10,9 +10,8 @@ HARVEST_API_URL = "http://fdp.dcc.sib.swiss"
 # I14Y API configuration
 ###########################################
 
-# API_BASE_URL = "https://api.i14y.admin.ch/api/partner/v1/" # Prod environement 
-API_BASE_URL = "https://api-a.i14y.admin.ch/api/partner/v1/" # ABN enironement 
-ACCESS_TOKEN = f"Bearer {os.environ['ACCESS_TOKEN']}" 
+API_BASE_URL = os.environ['API_BASE_URL'] 
+ACCESS_TOKEN = f"Bearer {os.environ['ACCESS_TOKEN']}"
 
 # Organization settings
 ORGANIZATION_ID = "CH_SPHN"
